31e22fcd5576c5ab06e02e690d76b86b59ae0c7d
David Huynh <dfhuynh@gmail.com>
2006-06-12 20:33:16 +0000

    Copied from longwell/trunk and mavenized. There is still a problem with jetty locking files.

---
+++ src/main/webapp/scripts/timeline-sources.js
@@ -0,0 +1,192 @@
+/*==================================================
+ *  Sorted Array
+ *==================================================
+ */
+
+Timeline.SortedArray = function(compare) {
+    this._a = [];
+    this._compare = compare;
+};
+
+Timeline.SortedArray.prototype.add = function(elmt) {
+    var sa = this;
+    var index = this.find(function(elmt2) {
+        return sa._compare(elmt2, elmt);
+    });
+    
+    if (index < this._a.length) {
+        this._a.splice(index, 0, elmt);
+    } else {
+        this._a.push(elmt);
+    }
+};
+
+Timeline.SortedArray.prototype.remove = function(elmt) {
+    var sa = this;
+    var index = this.find(function(elmt2) {
+        return sa._compare(elmt2, elmt);
+    });
+    
+    while (index < this._a.length && this._compare(this._a[index], elmt) == 0) {
+        if (this._a[index] == elmt) {
+            this._a.splice(index, 1);
+            return true;
+        } else {
+            index++;
+        }
+    }
+    return false;
+};
+
+Timeline.SortedArray.prototype.removeAll = function() {
+    this._a = [];
+};
+
+Timeline.SortedArray.prototype.elementAt = function(index) {
+    return this._a[index];
+};
+
+Timeline.SortedArray.prototype.length = function() {
+    return this._a.length;
+};
+
+Timeline.SortedArray.prototype.find = function(compare) {
+    var a = 0;
+    var b = this._a.length;
+    
+    while (a < b) {
+        var mid = Math.floor((a + b) / 2);
+        var c = compare(this._a[mid]);
+        if (mid == a) {
+            return c < 0 ? a+1 : a;
+        } else if (c < 0) {
+            a = mid;
+        } else {
+            b = mid;
+        }
+    }
+    return a;
+};
+
+/*==================================================
+ *  The All Selector
+ *==================================================
+ */
+
+
+Timeline.DefaultEventSource = function() {
+    this._events = new Timeline.SortedArray(Timeline.DefaultEventSource.compare);
+    this._listeners = [];
+};
+
+Timeline.DefaultEventSource.prototype.addListener = function(listener) {
+    this._listeners.push(listener);
+};
+
+Timeline.DefaultEventSource.prototype.removeListener = function(listener) {
+    for (var i = 0; i < this._listeners.length; i++) {
+        if (this._listeners[i] == listener) {
+            this._listeners.splice(i, 1);
+            break;
+        }
+    }
+};
+
+Timeline.DefaultEventSource.prototype.loadXML = function(xml) {
+    var node = xml.documentElement.firstChild;
+    var added = false;
+    while (node) {
+        if (node.nodeType == 1) {
+            var evt = new Timeline.DefaultEventSource.Event(
+                Timeline.parseGregorianDateTime(node.getAttribute("starts")),
+                Timeline.parseGregorianDateTime(node.getAttribute("ends")),
+                node.getAttribute("title"),
+                node.getAttribute("description")
+            );
+            this._events.add(evt);
+            
+            added = true;
+        }
+        node = node.nextSibling;
+    }
+    
+    if (added) {
+        for (var i = 0; i < this._listeners.length; i++) {
+            this._listeners[i].onAddMany();
+        }
+    }
+};
+
+Timeline.DefaultEventSource.prototype.clear = function() {
+    this._events.removeAll();
+    for (var i = 0; i < this._listeners.length; i++) {
+        this._listeners[i].onClear();
+    }
+};
+
+Timeline.DefaultEventSource.prototype.getEventIterator = function(startDate, endDate) {
+    return new Timeline.DefaultEventSource._Iterator(this._events, startDate, endDate);
+};
+
+Timeline.DefaultEventSource._Iterator = function(events, startDate, endDate) {
+    this._events = events;
+    this._startDate = startDate;
+    this._endDate = endDate;
+    
+    this._maxIndex = events.find(function(evt) {
+        return evt.getStart().getTime() - endDate.getTime();
+    });    
+    this._currentIndex = -1;
+    
+    this._hasNext = false;
+    this._next = null;
+    this._findNext();
+};
+
+Timeline.DefaultEventSource._Iterator.prototype = {
+    hasNext: function() { return this._hasNext; },
+    next: function() {
+        if (this._hasNext) {
+            var next = this._next;
+            this._findNext();
+            
+            return next;
+        } else {
+            return null;
+        }
+    },
+    _findNext: function() {
+        while ((++this._currentIndex) < this._maxIndex) {
+            var evt = this._events.elementAt(this._currentIndex);
+            if (evt.getStart().getTime() < this._endDate.getTime() &&
+                evt.getEnd().getTime() > this._startDate.getTime()) {
+                
+                this._next = evt;
+                this._hasNext = true;
+                return;
+            }
+        }
+        this._next = null;
+        this._hasNext = false;
+    }
+};
+
+Timeline.DefaultEventSource.compare = function(event1, event2) {
+    return event1.getStart().getTime() - event2.getStart().getTime();
+};
+
+Timeline.DefaultEventSource.Event = function(start, end, text, description) {
+    this._start = start;
+    this._end = (end != null) ? end : start;
+    this._text = text;
+    this._description = description;
+    this._id = "e" + Math.floor(Math.random() * 1000000);
+};
+
+Timeline.DefaultEventSource.Event.prototype = {
+    getStart:       function() { return this._start; },
+    getEnd:         function() { return this._end; },
+    getText:        function() { return this._text; },
+    getDescription: function() { return this._description; },
+    getID:          function() { return this._id; }
+};
---
+++ src/main/webapp/scripts/timeline.js
@@ -0,0 +1,602 @@
+/*==================================================
+ *  Timeline
+ *==================================================
+ */
+function Timeline(elmt, bandInfos, orientation) {
+    this._containerDiv = elmt;
+    this._orientation = orientation == null ? Timeline.HORIZONTAL : orientation;
+    this._bandInfos = bandInfos;
+    
+    this._initialize();
+};
+
+Timeline.HORIZONTAL = 0;
+Timeline.VERTICAL = 1;
+
+/*
+ *  Interval Units
+ */
+Timeline.MILLISECOND    = 0;
+Timeline.SECOND         = 1;
+Timeline.MINUTE         = 2;
+Timeline.HOUR           = 3;
+Timeline.DAY            = 4;
+Timeline.WEEK           = 5;
+Timeline.MONTH          = 6;
+Timeline.YEAR           = 7;
+Timeline.DECADE         = 8;
+Timeline.CENTURY        = 9;
+Timeline.MILLENNIUM     = 10;
+
+Timeline.GregorianUnitLengths = [];
+Timeline.GregorianUnitLengths[Timeline.MILLISECOND] = 1;
+Timeline.GregorianUnitLengths[Timeline.SECOND]      = 1000;
+Timeline.GregorianUnitLengths[Timeline.MINUTE]      = Timeline.GregorianUnitLengths[Timeline.SECOND] * 60;
+Timeline.GregorianUnitLengths[Timeline.HOUR]        = Timeline.GregorianUnitLengths[Timeline.MINUTE] * 60;
+Timeline.GregorianUnitLengths[Timeline.DAY]         = Timeline.GregorianUnitLengths[Timeline.HOUR] * 24;
+Timeline.GregorianUnitLengths[Timeline.WEEK]        = Timeline.GregorianUnitLengths[Timeline.DAY] * 7;
+Timeline.GregorianUnitLengths[Timeline.MONTH]       = Timeline.GregorianUnitLengths[Timeline.DAY] * 31;
+Timeline.GregorianUnitLengths[Timeline.YEAR]        = Timeline.GregorianUnitLengths[Timeline.DAY] * 365;
+Timeline.GregorianUnitLengths[Timeline.DECADE]      = Timeline.GregorianUnitLengths[Timeline.YEAR] * 10;
+Timeline.GregorianUnitLengths[Timeline.CENTURY]     = Timeline.GregorianUnitLengths[Timeline.YEAR] * 100;
+Timeline.GregorianUnitLengths[Timeline.MILLENNIUM]  = Timeline.GregorianUnitLengths[Timeline.YEAR] * 1000;
+
+Timeline.parseGregorianDateTime = function(o) {
+    if (o == null) {
+        return null;
+    } else if (o instanceof Date) {
+        return o;
+    } else {
+        try {
+            return new Date(Date.parse(o));
+        } catch (e) {
+            return null;
+        }
+    }
+}
+
+Timeline._isIE = (navigator.appName.indexOf("Microsoft") != -1);
+
+Timeline.prototype.layout = function() {
+    this._distributeWidths();
+};
+
+Timeline.prototype.getDocument = function() {
+    return this._containerDiv.ownerDocument;
+};
+
+Timeline.prototype.addDiv = function(div) {
+    this._containerDiv.appendChild(div);
+};
+
+Timeline.prototype.removeDiv = function(div) {
+    this._containerDiv.removeChild(div);
+};
+
+Timeline.prototype.isHorizontal = function() {
+    return this._orientation == Timeline.HORIZONTAL;
+};
+
+Timeline.prototype.isVertical = function() {
+    return this._orientation == Timeline.VERTICAL;
+};
+
+Timeline.prototype.getPixelLength = function() {
+    return this._orientation == Timeline.HORIZONTAL ? 
+        this._containerDiv.offsetWidth : this._containerDiv.offsetHeight;
+};
+
+Timeline.prototype.getPixelWidth = function() {
+    return this._orientation == Timeline.VERTICAL ? 
+        this._containerDiv.offsetWidth : this._containerDiv.offsetHeight;
+};
+
+Timeline.prototype._initialize = function() {
+    var containerDiv = this._containerDiv;
+    var doc = containerDiv.ownerDocument;
+    
+    containerDiv.className = 
+        containerDiv.className.split(" ").concat("timeline-container").join(" ");
+        
+    while (containerDiv.firstChild) {
+        containerDiv.removeChild(containerDiv.firstChild);
+    }
+    
+    this._bands = [];
+    for (var i = 0; i < this._bandInfos.length; i++) {
+        var band = new Timeline._Band(this, this._bandInfos[i]);
+        this._bands.push(band);
+    }
+    this._distributeWidths();
+    
+    for (var i = 0; i < this._bandInfos.length; i++) {
+        var bandInfo = this._bandInfos[i];
+        if ("highlight" in bandInfo) {
+            this._bands[i].setHighlightBand(this._bands[bandInfo.highlight]);
+        }
+    }
+};
+
+Timeline.prototype._distributeWidths = function() {
+    var length = this.getPixelLength();
+    var width = this.getPixelWidth();
+    var cumulativeWidth = 0;
+    
+    for (var i = 0; i < this._bands.length; i++) {
+        var band = this._bands[i];
+        var bandInfos = this._bandInfos[i];
+        var widthString = bandInfos.width;
+        
+        var x = widthString.indexOf("%");
+        if (x > 0) {
+            var percent = parseInt(widthString.substr(0, x));
+            var bandWidth = percent * width / 100;
+        } else {
+            var bandWidth = parseInt(widthString);
+        }
+        
+        band.setBandShiftAndWidth(cumulativeWidth, bandWidth);
+        band.setViewLength(length);
+        
+        cumulativeWidth += bandWidth;
+    }
+};
+
+Timeline.registerEventWithObject = function(elmt, eventName, obj, handler) {
+    Timeline.registerEvent(elmt, eventName, function(elmt2, evt, target) {
+        return handler.call(obj, elmt2, evt, target);
+    });
+};
+
+Timeline.registerEvent = function(elmt, eventName, handler) {
+    var handler2 = function(evt) {
+        evt = (evt) ? evt : ((event) ? event : null);
+        if (evt) {
+            var target = (evt.target) ? 
+                evt.target : ((evt.srcElement) ? evt.srcElement : null);
+            if (target) {
+                target = (target.nodeType == 1 || target.nodeType == 9) ? 
+                    target : target.parentNode;
+            }
+            
+            return handler(elmt, evt, target);
+        }
+        return true;
+    }
+    
+    if (Timeline._isIE) {
+        elmt.attachEvent("on" + eventName, handler2);
+    } else {
+        elmt.addEventListener(eventName, handler2, true);
+    }
+};
+
+/*==================================================
+ *  Band
+ *==================================================
+ */
+Timeline._Band = function(timeline, bandInfo) {
+    this._timeline = timeline;
+    this._bandInfo = bandInfo;
+    
+    this._dragging = false;
+    this._changing = false;
+    this._onScrollListeners = [];
+    
+    var b = this;
+    this._highlightBand = null;
+    this._highlightBandHandler = function(band) {
+        b._onHighlightBandScroll();
+    };
+    this._selectorListener = function(band) {
+        b._onHighlightBandScroll();
+    };
+    
+    this._div = this._timeline.getDocument().createElement("div");
+    this._div.className = "timeline-band";
+    this._timeline.addDiv(this._div);
+    
+    Timeline.registerEventWithObject(this._div, "mousedown", this, this._onMouseDown);
+    Timeline.registerEventWithObject(this._div, "mousemove", this, this._onMouseMove);
+    Timeline.registerEventWithObject(this._div, "mouseup", this, this._onMouseUp);
+    
+    this._innerDiv = this._timeline.getDocument().createElement("div");
+    this._innerDiv.className = "timeline-band-inner";
+    this._div.appendChild(this._innerDiv);
+    
+    this._ether = (bandInfo.ether) ? 
+        new bandInfo.ether(bandInfo.etherParams, timeline) : 
+        new Timeline.LinearEther(bandInfo.etherParams, timeline);
+        
+    this._etherPainter = (bandInfo.etherPainter) ? 
+        new bandInfo.etherPainter(bandInfo.etherPainterParams, this, timeline) : 
+        new Timeline.GregorianEtherPainter(bandInfo.etherPainterParams, this, timeline);
+        
+    this._eventSource = bandInfo.eventSource;
+    if (this._eventSource) {
+        this._eventSource.addListener({
+            onAddMany: function() { b._onAddMany(); },
+            onClear:   function() { b._onClear(); }
+        });
+    }
+    
+    this._eventPainter = (bandInfo.eventPainter) ? 
+        new bandInfo.eventPainter(bandInfo.eventPainterParams, this, timeline) : 
+        new Timeline.DurationEventPainter(bandInfo.etherPainterParams, this, timeline);
+};
+
+Timeline._Band.SCROLL_MULTIPLES = 5;
+
+Timeline._Band.prototype.addOnScrollListener = function(listener) {
+    this._onScrollListeners.push(listener);
+};
+
+Timeline._Band.prototype.removeOnScrollListener = function(listener) {
+    for (var i = 0; i < this._onScrollListeners.length; i++) {
+        if (this._onScrollListeners[i] == listener) {
+            this._onScrollListeners.splice(i, 1);
+            break;
+        }
+    }
+};
+
+Timeline._Band.prototype.setHighlightBand = function(band) {
+    if (this._highlightBand) {
+        this._highlightBand.removeOnScrollListener(this._highlightBandHandler);
+    }
+    
+    this._highlightBand = band;
+    this._highlightBand.addOnScrollListener(this._highlightBandHandler);
+    this._positionHighlight();
+};
+
+Timeline._Band.prototype.getEther = function() {
+    return this._ether;
+};
+
+Timeline._Band.prototype.getEventSource = function() {
+    return this._eventSource;
+};
+
+Timeline._Band.prototype.layout = function() {
+    this._etherPainter.paint();
+    this._paintEvents();
+};
+
+Timeline._Band.prototype.softLayout = function() {
+    this._etherPainter.softPaint();
+    this._softPaintEvents();
+};
+
+Timeline._Band.prototype.setBandShiftAndWidth = function(shift, width) {
+    if (this._timeline.isHorizontal()) {
+        this._div.style.top = shift + "px";
+        this._div.style.height = width + "px";
+    } else {
+        this._div.style.left = shift + "px";
+        this._div.style.width = width + "px";
+    }
+};
+
+Timeline._Band.prototype.getViewWidth = function() {
+    if (this._timeline.isHorizontal()) {
+        return this._div.offsetHeight;
+    } else {
+        return this._div.offsetWidth;
+    }
+};
+
+Timeline._Band.prototype.setViewLength = function(length) {
+    this._viewLength = length;
+    this._recenterDiv();
+    this._onChanging();
+};
+
+Timeline._Band.prototype.getViewLength = function() {
+    return this._viewLength;
+};
+
+Timeline._Band.prototype.getViewOffset = function() {
+    return this._viewOffset;
+};
+
+Timeline._Band.prototype.getMinDate = function() {
+    return this._ether.pixelOffsetToDate(this._viewOffset);
+};
+
+Timeline._Band.prototype.getMaxDate = function() {
+    return this._ether.pixelOffsetToDate(this._viewOffset + Timeline._Band.SCROLL_MULTIPLES * this._viewLength);
+};
+
+Timeline._Band.prototype.getMinVisibleDate = function() {
+    return this._ether.pixelOffsetToDate(0);
+};
+
+Timeline._Band.prototype.getMaxVisibleDate = function() {
+    return this._ether.pixelOffsetToDate(this._viewLength);
+};
+
+Timeline._Band.prototype.setMinVisibleDate = function(date) {
+    if (!this._changing) {
+        this._moveEther(Math.round(-this._ether.dateToPixelOffset(date)));
+    }
+};
+
+Timeline._Band.prototype.dateToPixelOffset = function(date) {
+    return this._ether.dateToPixelOffset(date) - this._viewOffset;
+};
+
+Timeline._Band.prototype.pixelOffsetToDate = function(pixels) {
+    return this._ether.pixelOffsetToDate(pixels + this._viewOffset);
+};
+
+Timeline._Band.prototype.createLayerDiv = function(zIndex) {
+    var div = this._timeline.getDocument().createElement("div");
+    div.className = "timeline-band-layer";
+    div.style.zIndex = zIndex;
+    this._innerDiv.appendChild(div);
+    
+    var innerDiv = this._timeline.getDocument().createElement("div");
+    innerDiv.className = "timeline-band-layer-inner";
+    div.appendChild(innerDiv);
+    
+    return innerDiv;
+};
+
+Timeline._Band.prototype.removeLayerDiv = function(div) {
+    this._innerDiv.removeChild(div.parentNode);
+};
+
+Timeline._Band.prototype._onMouseDown = function(innerFrame, evt, target) {
+    this._dragging = true;
+    this._dragX = evt.clientX;
+    this._dragY = evt.clientY;
+};
+
+Timeline._Band.prototype._onMouseMove = function(innerFrame, evt, target) {
+    if (this._dragging) {
+        var diffX = evt.clientX - this._dragX;
+        var diffY = evt.clientY - this._dragY;
+        
+        this._dragX = evt.clientX;
+        this._dragY = evt.clientY;
+        
+        this._moveEther(this._timeline.isHorizontal() ? diffX : diffY);
+        this._positionHighlight();
+    }
+};
+
+Timeline._Band.prototype._onMouseUp = function(innerFrame, evt, target) {
+    this._dragging = false;
+};
+
+Timeline._Band.prototype._moveEther = function(shift) {
+    this._viewOffset += shift;
+    this._ether.shiftPixels(-shift);
+    if (this._timeline.isHorizontal()) {
+        this._div.style.left = this._viewOffset + "px";
+    } else {
+        this._div.style.top = this._viewOffset + "px";
+    }
+    
+    if (this._viewOffset > -this._viewLength * 0.5 ||
+        this._viewOffset < -this._viewLength * (Timeline._Band.SCROLL_MULTIPLES - 1.5)) {
+        
+        this._recenterDiv();
+    } else {
+        this.softLayout();
+    }
+    
+    this._onChanging();
+}
+
+Timeline._Band.prototype._onChanging = function() {
+    this._changing = true;
+
+    this._fireOnScroll();
+    this._setHighlightBandDate();
+    
+    this._changing = false;
+};
+
+Timeline._Band.prototype._fireOnScroll = function() {
+    for (var i = 0; i < this._onScrollListeners.length; i++) {
+        this._onScrollListeners[i](this);
+    }
+};
+
+Timeline._Band.prototype._setHighlightBandDate = function() {
+    if (this._highlightBand) {
+        var centerDate = this._ether.pixelOffsetToDate(this.getViewLength() / 2);
+        var startDate = new Date(centerDate.getTime() - this._highlightBand.getEther().getDuration() / 2);
+        
+        this._highlightBand.setMinVisibleDate(startDate);
+    }
+};
+
+Timeline._Band.prototype._onHighlightBandScroll = function() {
+    if (this._highlightBand) {
+        var startDate = this._highlightBand.getMinVisibleDate();
+        var endDate = this._highlightBand.getMaxVisibleDate();
+        var centerDate = new Date(Math.round((startDate.getTime() + endDate.getTime()) / 2));
+        var centerPixelOffset = this._ether.dateToPixelOffset(centerDate);
+        
+        this._moveEther(Math.round(this._viewLength / 2 - centerPixelOffset));
+        this._etherPainter.setHighlight(startDate, endDate);
+    }
+};
+
+Timeline._Band.prototype._onAddMany = function() {
+    this._paintEvents();
+};
+
+Timeline._Band.prototype._onClear = function() {
+    this._paintEvents();
+};
+
+Timeline._Band.prototype._positionHighlight = function() {
+    if (this._highlightBand) {
+        var startDate = this._highlightBand.getMinVisibleDate();
+        var endDate = this._highlightBand.getMaxVisibleDate();
+        
+        this._etherPainter.setHighlight(startDate, endDate);
+    }
+};
+
+Timeline._Band.prototype._recenterDiv = function() {
+    this._viewOffset = -this._viewLength * (Timeline._Band.SCROLL_MULTIPLES - 1) / 2;
+    if (this._timeline.isHorizontal()) {
+        this._div.style.left = this._viewOffset + "px";
+        this._div.style.width = (Timeline._Band.SCROLL_MULTIPLES * this._viewLength) + "px";
+    } else {
+        this._div.style.top = this._viewOffset + "px";
+        this._div.style.height = (Timeline._Band.SCROLL_MULTIPLES * this._viewLength) + "px";
+    }
+    this.layout();
+};
+
+Timeline._Band.prototype._paintEvents = function() {
+    this._eventPainter.paint();
+};
+
+Timeline._Band.prototype._softPaintEvents = function() {
+    this._eventPainter.softPaint();
+};
+
+/*==================================================
+ *  Utilities
+ *==================================================
+ */
+
+Timeline.handleCaughtException = function(e) {
+    alert(e);
+};
+
+Timeline.loadXML = function(url, f) {
+    var fError = function(statusText, status, xmlhttp) {
+        alert("Failed to load data xml from " + url + "\n" + statusText);
+    };
+    var fDone = function(xmlhttp) {
+        f(xmlhttp.responseXML);
+    };
+    Timeline.xmlHttpGet(url, fError, fDone);
+};
+
+/**
+ *  Callback for XMLHttp onRequestStateChange.
+ */
+Timeline._xmlHttpRequest_onReadyStateChange = function(xmlhttp, fError, fDone) {
+    switch (xmlhttp.readyState) {
+    // 1: Request not yet made
+    // 2: Contact established with server but nothing downloaded yet
+    // 3: Called multiple while downloading in progress
+    
+    // Download complete
+    case 4:
+        try {
+            if (xmlhttp.status == 200) {
+                if (fDone) {
+                    fDone(xmlhttp);
+                }
+            } else {
+                if (fError) {
+                    fError(
+                        xmlhttp.statusText,
+                        xmlhttp.status,
+                        xmlhttp
+                    );
+                }
+            }
+        } catch (e) {
+            Timeline.handleCaughtException(e);
+        }
+        break;
+    }
+};
+
+/**
+ *  Creates an XMLHttpRequest object. On the first run, this
+ *  function creates a platform-specific function for
+ *  instantiating an XMLHttpRequest object and then replaces
+ *  itself with that function.
+ */
+Timeline._createXmlHttpRequest = function() {
+    if (Timeline._isIE) {
+        var programIDs = [
+        "Msxml2.XMLHTTP",
+        "Microsoft.XMLHTTP",
+        "Msxml2.XMLHTTP.4.0"
+        ];
+        for (var i = 0; i < programIDs.length; i++) {
+            try {
+                var programID = programIDs[i];
+                var f = function() {
+                    return new ActiveXObject(programID);
+                };
+                var o = f();
+                
+                // We are replacing the Timeline._createXmlHttpRequest
+                // function with this inner function as we've
+                // found out that it works. This is so that we
+                // don't have to do all the testing over again
+                // on subsequent calls.
+                Timeline._createXmlHttpRequest = f;
+                
+                return o;
+            } catch (e) {
+                // silent
+            }
+        }
+        throw new Error("Failed to create an XMLHttpRequest object");
+    } else {
+        try {
+            var f = function() {
+                return new XMLHttpRequest();
+            };
+            var o = f();
+            
+            // We are replacing the Timeline._createXmlHttpRequest
+            // function with this inner function as we've
+            // found out that it works. This is so that we
+            // don't have to do all the testing over again
+            // on subsequent calls.
+            Timeline._createXmlHttpRequest = f;
+            
+            return o;
+        } catch (e) {
+            throw new Error("Failed to create an XMLHttpRequest object");
+        }
+    }
+};
+
+/**
+ *  Performs an asynchronous HTTP GET.
+ *  fError is of the form function(statusText, statusCode, xmlhttp).
+ *  fDone is of the form function(xmlhttp).
+ */
+Timeline.xmlHttpGet = function(url, fError, fDone) {
+    var xmlhttp = Timeline._createXmlHttpRequest();
+    
+    xmlhttp.open("GET", url, true);
+    //xmlhttp.overrideMimeType("text/xml");
+    xmlhttp.onreadystatechange = function() {
+        Timeline._xmlHttpRequest_onReadyStateChange(xmlhttp, fError, fDone);
+    };
+    xmlhttp.send(null);
+};
+
+/**
+ *  Performs an asynchronous HTTP POST.
+ *  fError is of the form function(statusText, statusCode, xmlhttp).
+ *  fDone is of the form function(xmlhttp).
+ */
+Timeline.xmlHttpPost = function(url, body, fError, fDone) {
+    var xmlhttp = Timeline._createXmlHttpRequest();
+    
+    xmlhttp.open("POST", url, true);
+    //xmlhttp.overrideMimeType("text/xml");
+    xmlhttp.onreadystatechange = function() {
+        Timeline._xmlHttpRequest_onReadyStateChange(xmlhttp, fError, fDone);
+    };
+    xmlhttp.send(body);
+}

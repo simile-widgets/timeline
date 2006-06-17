e96260bd8287c847602f3647b851ab1594c6d693
David Huynh <dfhuynh@gmail.com>
2006-06-17 22:15:19 +0000

    First go at localizing ether interval labels.

---
+++ src/main/webapp/scripts/l10n/en/date-time.js
@@ -0,0 +1,10 @@
+/*==================================================
+ *  Localization of util/date-time.js
+ *==================================================
+ */
+
+Timeline.DateTime.gregorianMonthNames["en"] = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
+Timeline.DateTime.labelIntervalFunctions["en"] = Timeline.DateTime.defaultLabelInterval;

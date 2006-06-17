4bf867362dc28260c60af5c66e5100de564a4e96
dfhuynh <dfhuynh@579c892a-5248-0410-a714-2bcf037eb5bf>
2006-06-17 18:37:14 +0000

    Factored utility functions out to separate files.
    git-svn-id: http://simile-widgets.googlecode.com/svn/timeline/trunk@16 579c892a-5248-0410-a714-2bcf037eb5bf

--- src/main/webapp/scripts/timeline.js
+++ src/main/webapp/scripts/timeline.js
@@ -13,54 +13,12 @@ function Timeline(elmt, bandInfos, orientation) {
 Timeline.HORIZONTAL = 0;
 Timeline.VERTICAL = 1;
 
-/*
- *  Interval Units
- */
-Timeline.MILLISECOND    = 0;
-Timeline.SECOND         = 1;
-Timeline.MINUTE         = 2;
-Timeline.HOUR           = 3;
-Timeline.DAY            = 4;
-Timeline.WEEK           = 5;
-Timeline.MONTH          = 6;
-Timeline.YEAR           = 7;
-Timeline.DECADE         = 8;
-Timeline.CENTURY        = 9;
-Timeline.MILLENNIUM     = 10;
-
-Timeline.GregorianUnitLengths = [];
-Timeline.GregorianUnitLengths[Timeline.MILLISECOND] = 1;
-Timeline.GregorianUnitLengths[Timeline.SECOND]      = 1000;
-Timeline.GregorianUnitLengths[Timeline.MINUTE]      = Timeline.GregorianUnitLengths[Timeline.SECOND] * 60;
-Timeline.GregorianUnitLengths[Timeline.HOUR]        = Timeline.GregorianUnitLengths[Timeline.MINUTE] * 60;
-Timeline.GregorianUnitLengths[Timeline.DAY]         = Timeline.GregorianUnitLengths[Timeline.HOUR] * 24;
-Timeline.GregorianUnitLengths[Timeline.WEEK]        = Timeline.GregorianUnitLengths[Timeline.DAY] * 7;
-Timeline.GregorianUnitLengths[Timeline.MONTH]       = Timeline.GregorianUnitLengths[Timeline.DAY] * 31;
-Timeline.GregorianUnitLengths[Timeline.YEAR]        = Timeline.GregorianUnitLengths[Timeline.DAY] * 365;
-Timeline.GregorianUnitLengths[Timeline.DECADE]      = Timeline.GregorianUnitLengths[Timeline.YEAR] * 10;
-Timeline.GregorianUnitLengths[Timeline.CENTURY]     = Timeline.GregorianUnitLengths[Timeline.YEAR] * 100;
-Timeline.GregorianUnitLengths[Timeline.MILLENNIUM]  = Timeline.GregorianUnitLengths[Timeline.YEAR] * 1000;
-
-Timeline.parseGregorianDateTime = function(o) {
-    if (o == null) {
-        return null;
-    } else if (o instanceof Date) {
-        return o;
-    } else {
-        try {
-            return new Date(Date.parse(o));
-        } catch (e) {
-            return null;
-        }
-    }
-}
-
 Timeline.createSimpleBandInfo = function(width, intervalUnit, intervalCount, eventSource, date, etherCssClass, showEventText) {
     return {   
         width:          width,
         eventSource:    eventSource,
         etherParams: { 
-            duration: intervalCount * Timeline.GregorianUnitLengths[intervalUnit], 
+            duration: intervalCount * Timeline.DateTime.gregorianUnitLengths[intervalUnit], 
             centersOn: date 
         },
         etherPainterParams: { unit: intervalUnit, cssClass: etherCssClass },
@@ -68,48 +26,14 @@ Timeline.createSimpleBandInfo = function(width, intervalUnit, intervalCount, eve
     };
 };
 
-Timeline.isIE = false;
-Timeline.isWin = false;
-Timeline.isWin32 = false;
-Timeline.pngIsTranslucent = true;
-(function() {
-    Timeline.isIE= (navigator.appName.indexOf("Microsoft") != -1);
-    
-	var ua = navigator.userAgent.toLowerCase(); 
-	Timeline.isWin = (ua.indexOf('win') != -1);
-	Timeline.isWin32 = Timeline.isWin && (   
-        ua.indexOf('95') != -1 || 
-        ua.indexOf('98') != -1 || 
-        ua.indexOf('nt') != -1 || 
-        ua.indexOf('win32') != -1 || 
-        ua.indexOf('32bit') != -1
-    );
-    Timeline.pngIsTranslucent = !(Timeline.isIE && Timeline.isWin32);
-})();
-
-Timeline.createTranslucentImage = function(doc, url, width, height, verticalAlign) {
-    var elmt;
-    if (Timeline.pngIsTranslucent) {
-        elmt = doc.createElement("img");
-        elmt.setAttribute("src", url);
-        elmt.style.verticalAlign = (verticalAlign != null) ? verticalAlign : "text-top";
-    } else {
-        elmt = doc.createElement("div");
-        elmt.style.display = "inline";
-        elmt.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + url +"', sizingMethod='scale')";
-    }
-    elmt.style.width = width + "px";
-    elmt.style.height = height + "px";
-    return elmt;
-};
-Timeline.setOpacity = function(elmt, opacity) {
-    if (Timeline.isIE) {
-        elmt.style.filter = "progid:DXImageTransform.Microsoft.Alpha(Style=0,Opacity=" + opacity + ")";
-    } else {
-        var o = (opacity / 100).toString();
-        elmt.style.opacity = o;
-        elmt.style.MozOpacity = o;
-    }
+Timeline.loadXML = function(url, f) {
+    var fError = function(statusText, status, xmlhttp) {
+        alert("Failed to load data xml from " + url + "\n" + statusText);
+    };
+    var fDone = function(xmlhttp) {
+        f(xmlhttp.responseXML);
+    };
+    Timeline.XmlHttp.get(url, fError, fDone);
 };
 
 Timeline.prototype.layout = function() {
@@ -197,35 +121,6 @@ Timeline.prototype._distributeWidths = function() {
     }
 };
 
-Timeline.registerEventWithObject = function(elmt, eventName, obj, handler) {
-    Timeline.registerEvent(elmt, eventName, function(elmt2, evt, target) {
-        return handler.call(obj, elmt2, evt, target);
-    });
-};
-
-Timeline.registerEvent = function(elmt, eventName, handler) {
-    var handler2 = function(evt) {
-        evt = (evt) ? evt : ((event) ? event : null);
-        if (evt) {
-            var target = (evt.target) ? 
-                evt.target : ((evt.srcElement) ? evt.srcElement : null);
-            if (target) {
-                target = (target.nodeType == 1 || target.nodeType == 9) ? 
-                    target : target.parentNode;
-            }
-            
-            return handler(elmt, evt, target);
-        }
-        return true;
-    }
-    
-    if (Timeline.isIE) {
-        elmt.attachEvent("on" + eventName, handler2);
-    } else {
-        elmt.addEventListener(eventName, handler2, true);
-    }
-};
-
 /*==================================================
  *  Band
  *==================================================
@@ -251,9 +146,9 @@ Timeline._Band = function(timeline, bandInfo) {
     this._div.className = "timeline-band";
     this._timeline.addDiv(this._div);
     
-    Timeline.registerEventWithObject(this._div, "mousedown", this, this._onMouseDown);
-    Timeline.registerEventWithObject(this._div, "mousemove", this, this._onMouseMove);
-    Timeline.registerEventWithObject(this._div, "mouseup", this, this._onMouseUp);
+    Timeline.DOM.registerEventWithObject(this._div, "mousedown", this, this._onMouseDown);
+    Timeline.DOM.registerEventWithObject(this._div, "mousemove", this, this._onMouseMove);
+    Timeline.DOM.registerEventWithObject(this._div, "mouseup", this, this._onMouseUp);
     
     this._innerDiv = this._timeline.getDocument().createElement("div");
     this._innerDiv.className = "timeline-band-inner";
@@ -530,140 +425,3 @@ Timeline._Band.prototype._softPaintEvents = function() {
     this._eventPainter.softPaint();
 };
 
-/*==================================================
- *  Utilities
- *==================================================
- */
-
-Timeline.handleCaughtException = function(e) {
-    alert("Caught exception: " + e);
-};
-
-Timeline.loadXML = function(url, f) {
-    var fError = function(statusText, status, xmlhttp) {
-        alert("Failed to load data xml from " + url + "\n" + statusText);
-    };
-    var fDone = function(xmlhttp) {
-        f(xmlhttp.responseXML);
-    };
-    Timeline.xmlHttpGet(url, fError, fDone);
-};
-
-/**
- *  Callback for XMLHttp onRequestStateChange.
- */
-Timeline._xmlHttpRequest_onReadyStateChange = function(xmlhttp, fError, fDone) {
-    switch (xmlhttp.readyState) {
-    // 1: Request not yet made
-    // 2: Contact established with server but nothing downloaded yet
-    // 3: Called multiple while downloading in progress
-    
-    // Download complete
-    case 4:
-        try {
-            if (xmlhttp.status == 200) {
-                if (fDone) {
-                    fDone(xmlhttp);
-                }
-            } else {
-                if (fError) {
-                    fError(
-                        xmlhttp.statusText,
-                        xmlhttp.status,
-                        xmlhttp
-                    );
-                }
-            }
-        } catch (e) {
-            Timeline.handleCaughtException(e);
-        }
-        break;
-    }
-};
-
-/**
- *  Creates an XMLHttpRequest object. On the first run, this
- *  function creates a platform-specific function for
- *  instantiating an XMLHttpRequest object and then replaces
- *  itself with that function.
- */
-Timeline._createXmlHttpRequest = function() {
-    if (Timeline.isIE) {
-        var programIDs = [
-        "Msxml2.XMLHTTP",
-        "Microsoft.XMLHTTP",
-        "Msxml2.XMLHTTP.4.0"
-        ];
-        for (var i = 0; i < programIDs.length; i++) {
-            try {
-                var programID = programIDs[i];
-                var f = function() {
-                    return new ActiveXObject(programID);
-                };
-                var o = f();
-                
-                // We are replacing the Timeline._createXmlHttpRequest
-                // function with this inner function as we've
-                // found out that it works. This is so that we
-                // don't have to do all the testing over again
-                // on subsequent calls.
-                Timeline._createXmlHttpRequest = f;
-                
-                return o;
-            } catch (e) {
-                // silent
-            }
-        }
-        throw new Error("Failed to create an XMLHttpRequest object");
-    } else {
-        try {
-            var f = function() {
-                return new XMLHttpRequest();
-            };
-            var o = f();
-            
-            // We are replacing the Timeline._createXmlHttpRequest
-            // function with this inner function as we've
-            // found out that it works. This is so that we
-            // don't have to do all the testing over again
-            // on subsequent calls.
-            Timeline._createXmlHttpRequest = f;
-            
-            return o;
-        } catch (e) {
-            throw new Error("Failed to create an XMLHttpRequest object");
-        }
-    }
-};
-
-/**
- *  Performs an asynchronous HTTP GET.
- *  fError is of the form function(statusText, statusCode, xmlhttp).
- *  fDone is of the form function(xmlhttp).
- */
-Timeline.xmlHttpGet = function(url, fError, fDone) {
-    var xmlhttp = Timeline._createXmlHttpRequest();
-    
-    xmlhttp.open("GET", url, true);
-    //xmlhttp.overrideMimeType("text/xml");
-    xmlhttp.onreadystatechange = function() {
-        Timeline._xmlHttpRequest_onReadyStateChange(xmlhttp, fError, fDone);
-    };
-    xmlhttp.send(null);
-};
-
-/**
- *  Performs an asynchronous HTTP POST.
- *  fError is of the form function(statusText, statusCode, xmlhttp).
- *  fDone is of the form function(xmlhttp).
- */
-Timeline.xmlHttpPost = function(url, body, fError, fDone) {
-    var xmlhttp = Timeline._createXmlHttpRequest();
-    
-    xmlhttp.open("POST", url, true);
-    //xmlhttp.overrideMimeType("text/xml");
-    xmlhttp.onreadystatechange = function() {
-        Timeline._xmlHttpRequest_onReadyStateChange(xmlhttp, fError, fDone);
-    };
-    xmlhttp.send(body);
-}
---
+++ src/main/webapp/scripts/util/graphics.js
@@ -0,0 +1,34 @@
+/*==================================================
+ *  Graphics Utility Functions and Constants
+ *==================================================
+ */
+
+Timeline.Graphics = new Object();
+Timeline.Graphics.pngIsTranslucent = !(Timeline.Platform.isIE && Timeline.Platform.isWin32);
+
+Timeline.Graphics.createTranslucentImage = function(doc, url, width, height, verticalAlign) {
+    var elmt;
+    if (Timeline.Graphics.pngIsTranslucent) {
+        elmt = doc.createElement("img");
+        elmt.setAttribute("src", url);
+        elmt.style.verticalAlign = (verticalAlign != null) ? verticalAlign : "text-top";
+    } else {
+        elmt = doc.createElement("div");
+        elmt.style.display = "inline";
+        elmt.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + url +"', sizingMethod='scale')";
+    }
+    elmt.style.width = width + "px";
+    elmt.style.height = height + "px";
+    return elmt;
+};
+
+Timeline.Graphics.setOpacity = function(elmt, opacity) {
+    if (Timeline.Graphics.isIE) {
+        elmt.style.filter = "progid:DXImageTransform.Microsoft.Alpha(Style=0,Opacity=" + opacity + ")";
+    } else {
+        var o = (opacity / 100).toString();
+        elmt.style.opacity = o;
+        elmt.style.MozOpacity = o;
+    }
+};
+

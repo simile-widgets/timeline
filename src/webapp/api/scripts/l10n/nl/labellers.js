f237d10a0472ee9334c2c6d132213d983522b41c
larrykluger <larrykluger@579c892a-5248-0410-a714-2bcf037eb5bf>
2008-09-11 15:28:29 +0000

    TIMELINE: Added copy of JFK timeline in examples with Dutch locale set. Added forceLocale parameter to timeline-api.js to force a locale for testing. Added Dutch localization (Marko)
    git-svn-id: http://simile-widgets.googlecode.com/svn/timeline/trunk@1560 579c892a-5248-0410-a714-2bcf037eb5bf

---
+++ src/webapp/api/scripts/l10n/nl/labellers.js
@@ -0,0 +1,8 @@
+/*==================================================
+ *  Localization of labellers.js
+ *==================================================
+ */
+
+Timeline.GregorianDateLabeller.monthNames["nl"] = [
+ "Jan", "Feb", "Mrt", "Apr", "Mei", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"
+];

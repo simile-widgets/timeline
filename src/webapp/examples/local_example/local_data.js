e2f01fe9390847f8b87a5ebb612af81618a92f6a
Stefano Mazzocchi <stefano@apache.org>
2009-03-24 23:18:58 +0000

    get rid of the shelf wiki integration with the examples that is rarely used anyway

--- src/webapp/examples/local_example/local_data.js
+++ src/webapp/examples/local_example/local_data.js
@@ -1,7 +1,5 @@
 var timeline_data = {  // save as a global variable
 'dateTimeFormat': 'iso8601',
-'wikiURL': "http://simile.mit.edu/shelf/",
-'wikiSection': "Simile Cubism Timeline",
 
 'events' : [
         {'start': '1924',

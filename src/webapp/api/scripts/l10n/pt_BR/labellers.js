e49ff2d48f4ea6a59a36aa9560ef27ea215ab643
Larry Kluger <github@kluger.com>
2009-05-10 19:19:50 +0000

    Added pt_BR localization supplied by Edimar Manica -- LarryK

---
+++ src/webapp/api/scripts/l10n/pt_BR/labellers.js
@@ -0,0 +1,12 @@
+/*==================================================
+ *  Localization of labellers.js
+ *==================================================
+ */
+
+Timeline.GregorianDateLabeller.monthNames["en"] = [
+    "Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"
+];
+
+Timeline.GregorianDateLabeller.dayNames["en"] = [
+    "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"
+];
---
+++ src/webapp/api/scripts/l10n/pt_BR/timeline.js
@@ -0,0 +1,9 @@
+/*==================================================
+ *  Common localization strings
+ *==================================================
+ */
+
+Timeline.strings["pt_BR"] = {
+    wikiLinkLabel:  "Discuss"  // Using English until someone submits the pt_BR localization
+};
+

f3b9b4a9845c8f61ceab09de481af87ed969c237
David Huynh <dfhuynh@gmail.com>
2009-05-17 20:11:24 +0000

    TIMELINE Switched to using the same loading callback for all APIs so we can have a uniform way of loading a sequence of APIs. Added 2 examples of dynamic loading.

--- src/webapp/api/compile-epilog.js
+++ src/webapp/api/compile-epilog.js
@@ -1,11 +1,13 @@
 (function() {
     var f = null;
-    if (typeof Timeline_onLoad == "string") {
-        f = eval(Timeline_onLoad);
-        Timeline_onLoad = null;
-    } else if (typeof Timeline_onLoad == "function") {
-        f = Timeline_onLoad;
-        Timeline_onLoad = null;
+    if ("SimileWidgets_onLoad" in window) {
+        if (typeof SimileWidgets_onLoad == "string") {
+            f = eval(SimileWidgets_onLoad);
+            SimileWidgets_onLoad = null;
+        } else if (typeof SimileWidgets_onLoad == "function") {
+            f = SimileWidgets_onLoad;
+            SimileWidgets_onLoad = null;
+        }
     }
     
     if (f != null) {

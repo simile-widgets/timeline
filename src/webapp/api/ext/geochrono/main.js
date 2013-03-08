80541a4667f44badb67cf526bd543883e38e15e6
Ryan Lee <ryanlee@zepheira.com>
2013-03-07 21:12:59 -0800

    untested conversion of geochrono extension to RequireJS module

---
+++ src/webapp/api/ext/geochrono/main.js
@@ -0,0 +1,5 @@
+requirejs.config({
+    "paths": {
+        "simile-ajax": "http://localhost/~ryanlee/dev/simile-ajax/scripts/simile-ajax-api.js"
+    }
+});
--- src/webapp/api/ext/geochrono/scripts/units.js
+++ src/webapp/api/ext/geochrono/scripts/units.js
@@ -2,57 +2,57 @@
  *  Geochrono Unit
  *==================================================
  */
+define(["./labellers"], function(GeochronoLabeller) {
+var GeochronoUnit = new Object();
 
-Timeline.GeochronoUnit = new Object();
+GeochronoUnit.MA     = 0;
+GeochronoUnit.AGE    = 1;
+GeochronoUnit.EPOCH  = 2;
+GeochronoUnit.PERIOD = 3;
+GeochronoUnit.ERA    = 4;
+GeochronoUnit.EON    = 5;
 
-Timeline.GeochronoUnit.MA     = 0;
-Timeline.GeochronoUnit.AGE    = 1;
-Timeline.GeochronoUnit.EPOCH  = 2;
-Timeline.GeochronoUnit.PERIOD = 3;
-Timeline.GeochronoUnit.ERA    = 4;
-Timeline.GeochronoUnit.EON    = 5;
-
-Timeline.GeochronoUnit.getParser = function(format) {
-    return Timeline.GeochronoUnit.parseFromObject;
+GeochronoUnit.getParser = function(format) {
+    return GeochronoUnit.parseFromObject;
 };
 
-Timeline.GeochronoUnit.createLabeller = function(locale, timeZone) {
-    return new Timeline.GeochronoLabeller(locale);
+GeochronoUnit.createLabeller = function(locale, timeZone) {
+    return new GeochronoLabeller(locale);
 };
 
-Timeline.GeochronoUnit.wrapMA = function (n) {
-    return new Timeline.GeochronoUnit._MA(n);
+GeochronoUnit.wrapMA = function (n) {
+    return new GeochronoUnit._MA(n);
 };
 
-Timeline.GeochronoUnit.makeDefaultValue = function () {
-    return Timeline.GeochronoUnit.wrapMA(0);
+GeochronoUnit.makeDefaultValue = function () {
+    return GeochronoUnit.wrapMA(0);
 };
 
-Timeline.GeochronoUnit.cloneValue = function (v) {
-    return new Timeline.GeochronoUnit._MA(v._n);
+GeochronoUnit.cloneValue = function (v) {
+    return new GeochronoUnit._MA(v._n);
 };
 
-Timeline.GeochronoUnit.parseFromObject = function(o) {
-    if (o instanceof Timeline.GeochronoUnit._MA) {
+GeochronoUnit.parseFromObject = function(o) {
+    if (o instanceof GeochronoUnit._MA) {
         return o;
     } else if (typeof o == "number") {
-        return Timeline.GeochronoUnit.wrapMA(o);
+        return GeochronoUnit.wrapMA(o);
     } else if (typeof o == "string" && o.length > 0) {
-        return Timeline.GeochronoUnit.wrapMA(Number(o));
+        return GeochronoUnit.wrapMA(Number(o));
     } else {
         return null;
     }
 };
 
-Timeline.GeochronoUnit.toNumber = function(v) {
+GeochronoUnit.toNumber = function(v) {
     return v._n;
 };
 
-Timeline.GeochronoUnit.fromNumber = function(n) {
-    return new Timeline.GeochronoUnit._MA(n);
+GeochronoUnit.fromNumber = function(n) {
+    return new GeochronoUnit._MA(n);
 };
 
-Timeline.GeochronoUnit.compare = function(v1, v2) {
+GeochronoUnit.compare = function(v1, v2) {
     var n1, n2;
     if (typeof v1 == "object") {
         n1 = v1._n;
@@ -68,19 +68,21 @@ Timeline.GeochronoUnit.compare = function(v1, v2) {
     return n2 - n1;
 };
 
-Timeline.GeochronoUnit.earlier = function(v1, v2) {
-    return Timeline.GeochronoUnit.compare(v1, v2) < 0 ? v1 : v2;
+GeochronoUnit.earlier = function(v1, v2) {
+    return GeochronoUnit.compare(v1, v2) < 0 ? v1 : v2;
 };
 
-Timeline.GeochronoUnit.later = function(v1, v2) {
-    return Timeline.GeochronoUnit.compare(v1, v2) > 0 ? v1 : v2;
+GeochronoUnit.later = function(v1, v2) {
+    return GeochronoUnit.compare(v1, v2) > 0 ? v1 : v2;
 };
 
-Timeline.GeochronoUnit.change = function(v, n) {
-    return new Timeline.GeochronoUnit._MA(v._n - n);
+GeochronoUnit.change = function(v, n) {
+    return new GeochronoUnit._MA(v._n - n);
 };
 
-Timeline.GeochronoUnit._MA = function(n) {
+GeochronoUnit._MA = function(n) {
     this._n = n;
 };
 
+    return GeochronoUnit;
+});

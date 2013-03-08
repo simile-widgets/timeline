04e745e4beb681b7588e0887a29dd5b53780f012
Ryan Lee <ryanlee@zepheira.com>
2013-03-07 18:44:47 -0800

    untested conversion of planning extension to RequireJS module

--- src/webapp/api/ext/planning/scripts/units.js
+++ src/webapp/api/ext/planning/scripts/units.js
@@ -3,31 +3,32 @@
  *==================================================
  */
 
-Timeline.PlanningUnit = new Object();
+define(["labellers"], function(PlanningLabeller) {
+var PlanningUnit = new Object();
 
-Timeline.PlanningUnit.DAY     = 0;
-Timeline.PlanningUnit.WEEK    = 1;
-Timeline.PlanningUnit.MONTH   = 2;
-Timeline.PlanningUnit.QUARTER = 3;
-Timeline.PlanningUnit.YEAR    = 4;
+PlanningUnit.DAY     = 0;
+PlanningUnit.WEEK    = 1;
+PlanningUnit.MONTH   = 2;
+PlanningUnit.QUARTER = 3;
+PlanningUnit.YEAR    = 4;
 
-Timeline.PlanningUnit.getParser = function(format) {
-    return Timeline.PlanningUnit.parseFromObject;
+PlanningUnit.getParser = function(format) {
+    return PlanningUnit.parseFromObject;
 };
 
-Timeline.PlanningUnit.createLabeller = function(locale, timeZone) {
-    return new Timeline.PlanningLabeller(locale);
+PlanningUnit.createLabeller = function(locale, timeZone) {
+    return new PlanningLabeller(locale);
 };
 
-Timeline.PlanningUnit.makeDefaultValue = function () {
+PlanningUnit.makeDefaultValue = function () {
     return 0;
 };
 
-Timeline.PlanningUnit.cloneValue = function (v) {
+PlanningUnit.cloneValue = function (v) {
     return v;
 };
 
-Timeline.PlanningUnit.parseFromObject = function(o) {
+PlanningUnit.parseFromObject = function(o) {
     if (o == null) {
         return null;
     } else if (typeof o == "number") {
@@ -41,26 +42,28 @@ Timeline.PlanningUnit.parseFromObject = function(o) {
     }
 };
 
-Timeline.PlanningUnit.toNumber = function(v) {
+PlanningUnit.toNumber = function(v) {
     return v
 };
 
-Timeline.PlanningUnit.fromNumber = function(n) {
+PlanningUnit.fromNumber = function(n) {
     return n;
 };
 
-Timeline.PlanningUnit.compare = function(v1, v2) {
+PlanningUnit.compare = function(v1, v2) {
     return v1 - v2;
 };
 
-Timeline.PlanningUnit.earlier = function(v1, v2) {
-    return Timeline.PlanningUnit.compare(v1, v2) < 0 ? v1 : v2;
+PlanningUnit.earlier = function(v1, v2) {
+    return PlanningUnit.compare(v1, v2) < 0 ? v1 : v2;
 };
 
-Timeline.PlanningUnit.later = function(v1, v2) {
-    return Timeline.PlanningUnit.compare(v1, v2) > 0 ? v1 : v2;
+PlanningUnit.later = function(v1, v2) {
+    return PlanningUnit.compare(v1, v2) > 0 ? v1 : v2;
 };
 
-Timeline.PlanningUnit.change = function(v, n) {
+PlanningUnit.change = function(v, n) {
     return v + n;
 };
+    return PlanningUnit;
+});

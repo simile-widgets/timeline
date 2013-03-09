da7b397a59d2ba634ae35ef7d09815784d5cffab
Ryan Lee <ryanlee@zepheira.com>
2013-03-08 17:46:45 -0800

    updating example to use RequireJS form

---
+++ src/webapp/examples/compact-painter/main2.js
@@ -0,0 +1,74 @@
+requirejs.config({
+    "urlArgs": "bust=" + (new Date()).getTime(),
+    "baseUrl": "/timeline/api/",
+    "packages": [
+        {
+            "name": "simile-ajax",
+            "location": "/ajax/api",
+            "main": "local-config"
+        },
+        {
+            "name": "i18n",
+            "location": "/ajax/api/lib",
+            "main": "i18n"
+        }
+    ]
+});
+
+requirejs(
+    ["timeline-api"],
+    function(Timeline) {
+        var tl;
+        
+          var eventSource = new Timeline.DefaultEventSource(0);
+          
+          var theme = Timeline.ClassicTheme.create();
+          theme.event.instant.iconWidth += 4; // Add padding and borders
+          theme.event.instant.iconHeight += 4;
+          
+          var d = Timeline.DateTime.parseGregorianDateTime("1900")
+          var bandInfos = [
+              Timeline.createBandInfo({
+                  width:          "90%", 
+                  intervalUnit:   Timeline.DateTime.DECADE, 
+                  intervalPixels: 150,
+                  eventSource:    eventSource,
+                  date:           d,
+                  theme:          theme,
+                  eventPainter:   Timeline.CompactEventPainter,
+                  eventPainterParams: {
+                      iconWidth:      60,
+                      iconHeight:     60,
+                      iconLabelGap:   3
+                  }
+              }),
+              Timeline.createBandInfo({
+                  width:          "10%", 
+                  intervalUnit:   Timeline.DateTime.CENTURY, 
+                  intervalPixels: 100,
+                  eventSource:    eventSource,
+                  date:           d,
+                  theme:          theme,
+                  layout:         'overview'  // original, overview, detailed
+              })
+          ];
+          bandInfos[1].syncWith = 0;
+          bandInfos[1].highlight = true;
+          
+          tl = Timeline.create(document.getElementById("tl"), bandInfos, Timeline.HORIZONTAL);
+          tl.loadJSON("data2.json?"+ (new Date().getTime()), function(json, url) { eventSource.loadJSON(json, url); });
+
+        Timeline.writeVersion('tl_ver');
+
+        var resizeTimerID = null;
+        var onResize = function() {
+            if (resizeTimerID == null) {
+                resizeTimerID = window.setTimeout(function() {
+                    resizeTimerID = null;
+                    tl.layout();
+                }, 500);
+            }
+        };
+        window.onresize = onResize;
+    }
+);

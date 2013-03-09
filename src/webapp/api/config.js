f69532110f0d018af9be667e454e80d86d65eaa5
Ryan Lee <ryanlee@zepheira.com>
2013-03-08 22:19:21 -0800

    adapting based on actual use

--- src/webapp/api/config.js
+++ src/webapp/api/config.js
@@ -3,16 +3,15 @@ var timeline_version = "3.0.0";
 var ajax_version = "3.0.0";
 requirejs.config({
     "baseUrl": domain + "timeline/" + timeline_version + "/",
+    "paths": {
+        "jquery": domain + "ajax/" + ajax_version + "/lib/jquery",
+        "i18n": domain + "ajax/" + ajax_version + "/lib/i18n"
+    },
     "packages": [
         {
             "name": "simile-ajax",
             "location": domain + "ajax/" + ajax_version,
             "main": "config"
-        },
-        {
-            "name": "i18n",
-            "location": domain + "ajax/" + ajax_version + "/lib",
-            "main": "i18n"
         }
     ]
 });

02b45a5762bf9b080f2940b0795f4795b0e24c6c
Ryan Lee <ryanlee@zepheira.com>
2013-08-02 23:43:48 -0700

    removing jquery, not used in timeline at all

--- optimize/build-require.js
+++ optimize/build-require.js
@@ -3,9 +3,8 @@
     "name": "timeline",
     "out": "../build/timeline-bundle.js",
     "paths": {
-        "jquery": "lib/jquery",
         "i18n": "lib/i18n",
         "simile-ajax": "empty:"
     },
-    "exclude": ["jquery", "i18n"]
+    "exclude": ["i18n"]
 })

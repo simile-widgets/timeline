1549dbd23cc6a49615e3bd10b5255f669278a510
Ryan Lee <ryanlee@zepheira.com>
2013-08-02 23:15:17 -0700

    adding optimizer and configurations along with supporting libraries and build jobs, plus necessary changes to code for optimizer to run

--- src/webapp/api/scripts/base.js
+++ src/webapp/api/scripts/base.js
@@ -2,7 +2,7 @@ define(["simile-ajax"], function(SimileAjax) {
     var Timeline = {
         // Note: version is also stored in the build.xml file
         "version": "3.0.0",  // use format 'pre 1.2.3' for trunk versions
-        "ajax_lib_version": SimileAjax.version,
+        "ajax_lib_version": "3.0.0",
         "display_version": null,
         "HORIZONTAL": 0,
         "VERTICAL": 1,
@@ -10,19 +10,19 @@ define(["simile-ajax"], function(SimileAjax) {
         "urlPrefix": null,
         "serverLocale": null,
         "clientLocale": null,
-        "timelines": null
+        "timelines": null,
+        "params": {
+            "bundle": true,
+            "ajax": null
+        },
+        "paramTypes": {
+            "bundle": Boolean,
+            "ajax": String
+        }
     };
 
     // cf method Timeline.writeVersion
     Timeline.display_version = Timeline.version + ' (with Ajax lib ' + Timeline.ajax_lib_version + ')';
-
-    Timeline.getDefaultLocale = function() {
-        return Timeline.clientLocale;
-    };
-
-    Timeline.getTimelineFromID = function(timelineID) {
-        return Timeline.timelines[timelineID];
-    };
 
     return Timeline;
 });
